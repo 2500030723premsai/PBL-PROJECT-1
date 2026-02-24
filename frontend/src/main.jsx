cadb00c1fb9ce2cd895009195b287307c1659f67
2500030723 <premsaisree@gmail.com>
2026-02-24 09:56:38 +0530

    Fix GitHub Pages deployment: add basename and 404 handler, remove unused files

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,7 +7,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/PBL-PROJECT-1">
       <AppProvider>
         <App />
       </AppProvider>
